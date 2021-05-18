14cafb4629d95af042fbac58702646d471ee4b7d
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-05-18 19:24:01 -0300

    haciendo ejercicios

---
+++ demo8.js
@@ -0,0 +1,52 @@
+var juegos= {juegoActual:'batman',juegoJugado:'jailbreak',juegoFuturo: 'batmanakaman2'}
+
+
+
+console.log(juegos.juegoActual)
+
+
+
+
+
+// 2 -  Crear una funcion llamada getNameGames y que imprima alguna propiedad del objeto creado anteriormente.
+
+
+
+
+
+
+
+function  getNameGames () {
+
+
+console.log(juegos.juegoFuturo)
+
+if(juegos.juegoJugado==='jailbreak') {
+    console.log(juegos.juegoJugado)
+}
+}
+
+getNameGames()
+
+
+// 3 - Crear un if (condicional) dentro del cuerpo de la funcion, el if debe preguntar si la propiedad del objecto juegoJugado es igual al 
+// valor que le pusieron, y de ser asi que el if imprima el valor de la propiedad
+
+
+
+// 1 crear un objeto que se llame simbolo con tres propiedades, con los siguientes nombres:   numero, nombre y boolean.
+// la propiedad numero debe tener un numero natural
+// la propiedad nombre debe contener un string
+// la propiedad boolean debe contener un valor true o false (escribirlo sin comilla)
+
+
+
+
+
+
+
+var  simbolo= {
+
+    numero:2000000,boolean:false, nombre:'valen'
+
+}
---
+++ demo9.js
@@ -0,0 +1,58 @@
+var juegos=['Batman','Arsenal','BatmanAkaman2']
+
+
+console.log(juegos[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*crear un Array con el nombre chipote que contenga tres valores
+
+el valor 1 es un numero entero
+el valor 2 es un string
+el valor 3 es un boolean (se acuerdan? true o false si commillas)
+
+imprimir los tres valores, primero el boolen, luego el entero y por ultimo el string*/
+
+
+
+
+var chipote=[1000000,'valentino es pro',true]
+
+
+/*console.log(chipote[2])
+console.log(chipote[0])
+console.log(chipote[1])*/
+
+
+
+
+
+
+// Crear un Array con el nombre yanque que contenga tres valores
+
+/*el valor 1 es un objeto asi: { name: "Chipote Chillon" }
+el valor 2 es un entero
+el valor 3 es un boolean (se acuerdan? true o false)
+
+imprimir los tres valores, primero el boolen, luego el entero y por ultimo la propiedad name del obj*/
+
+var yanque=[ {name:'chipote chillon', juego:"Valne"},200000000000,false]
+
+/*console.log(yanque[2])
+console.log(yanque[1])*/
+console.log(yanque[0].name)
