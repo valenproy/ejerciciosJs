11f3e9a02cbe527c24dcd499062e1690ae52055a
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-05-31 20:00:03 -0300

    ejersisios js

---
+++ demo10.js
@@ -0,0 +1,23 @@
+// Crear si un array con el nombre  fps con tres elementos se acurdan como se hacia? [ elemento, elemento, elemeto],
+// el primer elemento se va a llamar frame
+// el segundo elemento se va llamr per
+// y el tercero seconds
+
+
+// Crear un Obj  que se llame roblox  con tres propiedades fps, personaje, escenario
+
+
+// crear una variable normal que contenga un string con el nombre name
+// crear otra variable normal que contengo un entero con el nombre number
+// cerar otro variable normal tipo boolen con el valor true
+
+
+var fps = ['frame','per','seconds']
+
+
+
+var roblox = {fps:'240',personaje:'monkey',ecenario:'disparos'}
+
+var name = 'castlers'
+var number = 200000000000000000000000
+var name = true
