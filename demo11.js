8724fd6f441841389d31884f22158a445dd8a119
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-06-01 19:40:23 -0300

    Nuevo ejercio con pizza pedorra quemada

---
+++ demo11.js
@@ -0,0 +1,119 @@
+// Crear  un array con el nombre tipos, con tres elementos se acurdan como se hacia? [ elemento, elemento, elemeto],
+// el primer elemento debe ser un string
+// el segundo elemento debe ser un entero 
+// el tercer elemento debe ser un boolen con valor false
+//imprimir primero el valor string, luego el boolean y por ultimo el entero
+
+//Crear un variable plana que se llame name y guardar un string
+//Imprimir la Variable
+
+
+// Obtener el valor entero del array y guardarlo en otra variable que se llame savedNumber
+// Imprimir la variable savedNumber
+
+
+
+
+
+var tipos = ['valentino es pro en arsenal',20000000000000000644546456656566565,false]
+
+console.log(tipos[0])
+console.log(tipos[2])
+console.log(tipos[1])
+
+
+var name = 'guason vs batman'
+
+console.log(name)
+
+var savedNumber=tipos[1]
+console.log(savedNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Crear una funcion que se llame comerPizza 
+// dentro de la funcion crear una variable plana que se llame gusto que sea tipo string, con el valor riquisima!
+// llamar la funcion, y dentro de la misma imprimir el valor de la variable riquisima
+
+
+// crear dentro de la funcion otra variable plana que se llame comiMucho con un valor string si
+//  crear un if que pregumte si la variable comiMucho es igual si, y de ser asi imprimir el valor de la variable
+
+
+// crear una variable tipo objeto que se llame programacion y que tenga dos propiedades
+// lenguaje1 y lenguaje2 y asignarles a cada una el nombre de un lenguaje como string
+
+
+
+
+function comerPizza() {
+
+var gusto = 'riqusima'
+
+
+console.log(gusto)
+
+
+var comiMucho = 'si'
+
+if(comiMucho==='si')
+console.log(comiMucho)
+}
+
+comerPizza()
+
+
+
+
+
+
+
+
+
+
+var programacion = {lenguage1:'html',lenguaje2:'js'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
