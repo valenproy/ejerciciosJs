a1bc24da6599daff640bb977041d6f9ffe3678ec
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-06-03 19:29:45 -0300

    ejercicios

---
+++ demo12.js
@@ -0,0 +1,51 @@
+  // Crear un array con dos elementos:
+  // Un elemento tipo string con el nombre "batman"
+  // Un elemento tipo int con el varlor 1
+  // imprimir los dos elementos con console log
+
+ 
+  // Crear un if que pregunte si el elemento numero 1 del array es iugal a batman
+  // Si es asi imprimir el en console log "Si es Batman!
+  
+
+  // Crear un objecto llamado pais con dos propiedades provincia1 y provincia2
+  // cada propiedad debe tener como valor un string con el nombre de uuna provincia de argentina
+  // imprimir alguna propiedad con console log
+
+
+
+  // Solo responder si es array, objeto, boolean, entero o string.
+  // cuando a una variable se le asinga esto {}, que se le esta asignando ? ¡ojeto! 
+  // cuando a una variable se le asinga esto [], que se le esta asignando ? ¡array!
+  // Cuando a una variable se le asiga esto "Soy Chipote chipoton", que tipo de valor es? ¡string!
+  // Cuando a una varialbe  se le asigna 89, que tipo de valor es? ¡entero!
+  // Cuando a una variable se la asingna el valor false o true, que tipo de valor es? ¡boolean!
+
+
+
+
+
+  var batman = ['batman',1]
+
+
+  console.log(batman[1])
+
+  if( batman[0] ==='batman'){
+      console.log('si es batman')
+
+
+
+
+
+  }
+
+
+var pais = {provincia1:'tierra del fuego',provincia2:'tucuman'}
+
+console.log(pais.provincia1)
+
+
+
+
+var pruevas = []
+
---
+++ demo13.js
@@ -0,0 +1,13 @@
+// Crear un array con el nombre prueba con tres elementos:
+  // el elemento 1 va a contener el valor string "Muy Chipi"
+  // el elemento 2 va contener el valor int 89
+  // el elemento 3 va a contener un objecto con la propiedad genios y el valor string maxivalen
+  // Ayudita:, deberia ir asi en la posicion 3 del array { genios:"maxivalen" }
+
+
+  // imprimir los tres valres de los elementos con console log
+
+
+  var pruevas = ['muy chipi',89,{genios:'maxiyvalen'}]
+
+  console.log(pruevas[0],pruevas[1],pruevas[2])
