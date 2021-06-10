62014742aefde021a0d3b7bde8b7362f8c67a802
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-06-10 19:23:32 -0300

    ejercicios de batman[C

---
+++ demo14.js
@@ -0,0 +1,51 @@
+// Crear un array con el nombre prueba con tres elementos:
+  // el elemento 1 va a contener el valor string "Batman"
+  // el elemento 2 va contener el valor int 899
+  // el elemento 3 va a contener un objecto con la propiedad 
+  // Ayudita:, deberia ir asi en la posicion 3 del array { juego1:"batman1",  juego2:"batman2" }
+
+
+  // imprimir el valor del elemento  0
+  // imprimir el valor del elemento  1
+  // imprimir el valor  batman2 sdel elemento  2
+
+
+  var prueba = ['batman',899, {juego1:'batman1', juego2:'batman2'}]
+
+  console.log(prueba[0])
+  console.log(prueba[1])
+  console.log(prueba[2])
+
+  var susiosMatones =  prueba[2]
+  
+   console.log(susiosMatones.juego2)
+
+    // Crear un array con el nombre prueba con tres elementos:
+  // el elemento 1 va a contener el valor string "Batman"
+  // el elemento 2 va contener el valor boolen true
+  // el elemento 3 va a contener un entero con el valor 1000
+  // crear una variable plana que se llama suma y asignarle un entero y sumarle la propiedad tres del array
+  //imprimir el resultado
+
+
+  // imprimir el valor del elemento  0
+  // imprimir el valor del elemento  1
+  // imprimir el valor del elemento  2
+
+  
+  var prueva = ['batman',true,1000]
+// Crear un un objeto que se llama numeros  con dos propiedades entero1 y entero2 
+  // la propiedad entero1 tiene que tener el valor 10
+  // la proiedad entero2 tiene que tener el valor 30
+// crear una variable plana que se llame suma y asingnarle la suma de la propiedad entero1 y la propiedad entero2
+// imprimir la variable suma
+  var  suma = 1 + prueva[2]
+  console.log(suma)
+  
+
+  var  numeros  =  {entero1:10,entero2:30}
+
+
+  var   suma =  numeros.entero1 + numeros.entero2
+  console.log(suma)
+  
