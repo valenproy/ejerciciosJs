53fa9f8e361d905af3416409489994043c7a6539
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-06-24 19:17:51 -0300

    ejercicios

---
+++ demo15.js
@@ -0,0 +1,91 @@
+// Crear un array de cuatro elementos llamado chavoDel8
+// el primer elemento va ser un entero con el numero 8
+// el segundo elemento va ser un string con el nombre "Programa Mexicano"
+// el tercer elemento va a ser un objeto con tres propiedades { pers1: 'chavo', pers2:'Don Ramon', pers:'Doña Florinda' }
+// imprimir el elemento 2
+// imprimir el valor Don Ramon del elemento 3
+
+
+// crear un obj con el nombre juegos con tres propiedades
+// juego1, juego2, juego3
+// Cada propiedad debe contener un  valor string con el nombre de un juego que les guste
+
+//imprimir juego2
+
+
+var chavoDel8 = [8 , 'progama mexicano' , {pers1: 'chavo', pers2: 'don ramon', pers3: 'doña florinda'}]
+
+
+console.log(chavoDel8[1])
+console.log(chavoDel8[2].pers2)
+
+
+var juegos = {juegos1:'batman', juegos2: 'batman2', juegos3: 'batman3'}
+ console.log(juegos.juegos2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Se crear un array que se llame juegos con 8 elementos, cada elemento del array debe ser un 
+// string con el nombre del juego que a uds les guste. 
+
+
+
+
+var juego = ['batman' , 'half life', 'robllox','jailbreak', 'adop me', 'funki friday', 'romperse los huesos', 'arsenal']
+
+
+
+
+
+
+
+juego.forEach( function ( element, index )  {
+    console.log(index)
+    console.log(element)
+    
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ demo16.js
@@ -0,0 +1,43 @@
+// crear un array vacio que se llame juegos 
+  // Crear una function que se llame  crearJuego
+  // dentro de la funcion colocar el siguiente codigo que agrega elementos al array: 
+  // juegos.push("Aqui el codigo")
+  // imprimir el array juegos
+  // Llammar a la function
+
+
+
+
+  var juegos = []
+
+  function crearJuego (juego) {
+    juegos.push(juego)
+
+console.log(juegos)
+console.log(juegos[3])
+  } 
+  
+
+
+
+
+
+
+
+  
+ // Llamar la funcion 4 veces
+ // la primera vez llamarla pasandole un boolean
+ // la sengunda Vez llamarla pasandole un String
+ // la tercera vez llamarla  pasarle un entero
+ // la cuarta vez llamarla  pasandole un objeto  con una propiedad llamda nomre y asignarle un nombre ej: {nomre:'fede'} 
+ // dentro de la funcion imprimir el elemento numero cuatro del array
+
+
+ 
+
+ crearJuego(false)
+ crearJuego('gata fiera')
+ crearJuego(535353)
+ crearJuego({nombre:'fede'})
+
+
