c520378bf5cbddc34a39128f4ec26c35b687afff
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-07-07 19:25:53 -0300

    puta consola

---
+++ demo17.js
@@ -0,0 +1,110 @@
+// crear un array vacio que se llame juegos 
+  // Crear una function que se llame  crearJuego
+  // dentro de la funcion colocar el siguiente codigo que agrega elementos al array: 
+  // juegos.push("Aqui el codigo")
+  // imprimir el array juegos
+  // Llammar a la function 
+
+
+var juegos = []
+
+function crearJuego (valores){
+
+    juegos.push(valores)
+
+}
+
+
+
+
+
+
+
+
+ // Llamar la funcion 4 veces
+ // la primera vez llamarla pasandole un boolean
+ // la sengunda Vez llamarla pasandole un String
+ // la tercera vez llamarla  pasarle un entero
+ // la cuarta vez llamarla  pasandole un objeto  con una propiedad llamda nomre y asignarle un nombre ej: {nomre:'fede'} 
+ // dentro de la funcion imprimir el elemento numero cuatro del array
+
+
+
+
+
+
+
+
+
+ crearJuego(true)
+ crearJuego('batman no le pegan ninguna bala porque es un mursielago')
+ crearJuego(100000)
+ crearJuego({nomre:'fede'})
+ 
+ 
+ console.log(juegos[3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Llamar la funcion 4 veces
+ // la primera vez llamarla pasandole un boolean
+ // la sengunda Vez llamarla pasandole un String
+ // la tercera vez llamarla  pasarle un entero
+ // la cuarta vez llamarla  pasandole un objeto  con una propiedad llamda nomre y asignarle un nombre ej: {nomre:'fede'} 
+ // dentro de la funcion imprimir el elemento numero cuatro del array
+//Hola chicos soy Federico el mas chipote chipoton aca les voy
+ // a pasar un ejercicio super yanque lleno de exito por que la verdad
+ // batamn y robin son muy pelotudos, mas pelotudo es robin y la verdad
+ // que a ese si le pegan las balas. Ademas no lo aguanto mas
+ // a la chipoteada por que es muy chipotaaa.
+ // cualquier cosa me avisan por que aca va el ejercicio:
+
+ // Crear un array con 4 elementos dos strings y dos enteros
+// cada elemento insertarlo con push
+// imprimir el array
+
+
+
+
+
+
+
+
+var boludos = []
+boludos.push('pedo')
+boludos.push('pedo')
+boludos.push(120)
+boludos.push(120)
+
+console.log(boludos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
