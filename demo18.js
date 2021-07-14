99dcc130b04872b574aa81306c16fc8b5a999795
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-07-14 19:13:34 -0300

    ejercicios ojetos

---
+++ demo18.js
@@ -0,0 +1,51 @@
+// Crear un array con tres elementos, el primer elemento es un boolean
+  // el segundo elemento es un string con el valor  'pumptrack'
+  // el tercer elemento del array es un objeto, que contendra una propiedad llamada juego, y le asignaran un string con el nombre
+  // de un juego que les guste.
+  
+  // imprimir la el tercer elemento
+
+
+  var elementos = [true , 'pumptrack', {juego:'bats'}]
+
+  console.log(elementos[2])
+
+
+
+
+
+
+
+
+  // crear un Objeto llamado starWars con tres propiedades amigo, enemigo y arma
+  // a cada propiedad del objeto asignarle un string 
+  // imprimir el varlos arma y amigo
+
+
+
+  var starWars = {amigo:'rey', enemigo:'Darth Vader', arma:'sable'}
+
+  console.log(starWars.amigo)
+
+
+
+
+
+
+   // crear un objeto simbolos que tenga tres propiedades, numero1, numero2 y verdadero
+ // a la propiedad numero1 asignarle el entero 12 y a la propiedad numero2 asignarle el numero entero 20
+ // a la propiedad verdadero asingarle un boolean
+
+ // sumar las propiedades de numero1 y dos e imprimirlas
+
+
+
+
+
+
+
+
+  var simbolos = {numero1:12, numero2:20,verdadero:false}
+
+
+  console.log(simbolos.numero1+simbolos.numero2)
