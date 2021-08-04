d5268b363230d03cc624aff5c1ceaa1706a96d0e
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-08-04 19:04:42 -0300

    ejercicios demo 19

---
+++ demo19.js
@@ -0,0 +1,29 @@
+//# ejerciciosJs
+
+/*Crear un Objeto llamado  options con dos propiedades entrar y otra denegar
+la propiedad denegar debe tener el valor boolean false y la propiedad entrar va a tener el valor true
+
+crear un if donde consulte si la propiedad entrar del objeto options es igual a true,  de ser asi que imprimir un console log con 
+un texto que diga: 'si es true la propiedad entrar'
+
+
+crear otro if donde consulte si la propiedad denegar del objeto options es igual a false,  de ser asi que imprimir un console log con 
+un texto que diga: 'si es false la propiedad denegar'*/
+
+
+var options  = {entrar: true, detener: false}
+
+
+
+if(options.entrar===true){
+
+console.log('si es true la propiedad entrar')
+
+}
+
+if(options.detener===false){
+
+    console.log('si es false la propiedad denegar')
+
+
+}
